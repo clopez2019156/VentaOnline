ec644eda98aea7fa249a05535ecd0f009606f8a5
Cesar López <clopez-2019156@kinal.edu.gt>
2021-03-12 18:24:31 -0600

    Se subió el proyecto

---
+++ src/rutas/factura.rutas.js
@@ -0,0 +1,11 @@
+'use strict'
+var express = require("express");
+var facturaControlador = require("../controladores/factura.controlador");
+var md_autorizacion = require("../middlewares/authenticated");
+
+var api = express.Router();
+
+api.post("/CrearFactura", facturaControlador.CrearFactura);
+api.delete("/CancelarFactura", facturaControlador.CancelarFactura);
+api.put('/FinalzarFactura', facturaControlador.FinalzarFactura);
+module.exports = api;
